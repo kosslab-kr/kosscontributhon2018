3de280583285e9e69e67627c7c9e57cb7e1606f6
Dongwoo Gim <gimdongwoo@gmail.com>
2018-08-18 04:44:03 +0900

    add next

---
+++ next.config.js
@@ -0,0 +1,28 @@
+const withPlugins = require('next-compose-plugins');
+const withLess = require('@zeit/next-less');
+const withSass = require('@zeit/next-sass');
+const withTypescript = require('@zeit/next-typescript');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+module.exports = withPlugins([
+  [
+    withTypescript,
+    {
+      webpack(config, options) {
+        // Do not run type checking twice:
+        if (options.isServer) config.plugins.push(new ForkTsCheckerWebpackPlugin());
+
+        return config;
+      },
+    },
+  ],
+  [withSass],
+  [
+    withLess,
+    {
+      lessLoaderOptions: {
+        javascriptEnabled: true,
+      },
+    },
+  ],
+]);
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+interface IProps {}
+
+class App extends React.Component<IProps> {
+  render() {
+    return <div>Hello world!</div>;
+  }
+}
+
+export default App;
