a8def0089b8f81a4cff5b36c2004ffd41ba739b0
Dongwoo Gim <gimdongwoo@gmail.com>
2018-08-20 18:36:36 +0900

    add query for `/api/events?type=` (support comma words)

--- server/handler/events.ts
+++ server/handler/events.ts
@@ -1,14 +1,17 @@
 import { ParseType } from './@type';
 
-async function getEvents(Parse: ParseType) {
+async function getEvents(Parse: ParseType, query: { type: string }) {
   const eventQ = new Parse.Query('Event');
   eventQ.descending('created_at');
+  if (query.type) {
+    eventQ.containedIn('type', query.type.split(','));
+  }
   const events = await eventQ.find();
   return events;
 }
 
 const exported = {
-  getEvents: (req, res) => getEvents(req.Parse).then(events => res.json(events)),
+  getEvents: (req, res) => getEvents(req.Parse, req.query).then(events => res.json(events)),
 };
 
 export default exported;
