4b138c74879b57582281c6ba6d0a902a046de830
Dongwoo Gim <gimdongwoo@gmail.com>
2018-08-25 15:38:04 +0900

    modify crawl timing

--- server/server.ts
+++ server/server.ts
@@ -139,7 +139,7 @@ function addCron() {
 
     // backgroundJob
     crond.putJob('backgroundJob', null);
-    crond.addCron('backgroundJob', '0 */5 * * * *'); // per 5 minutes
+    crond.addCron('backgroundJob', '0 */15 * * * *'); // per 15 minutes
 
     // start
     crond.start();
